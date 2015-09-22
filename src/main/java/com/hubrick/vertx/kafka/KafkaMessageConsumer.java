9738f7eb0e499321d0c261ead7a8f696dd8dc595
Marcus Thiesen <mt@hubrick.com>
2015-09-22 10:55:17 +0200

    Vert.x 3 port

--- src/main/java/com/hubrick/vertx/kafka/KafkaMessageConsumer.java
+++ src/main/java/com/hubrick/vertx/kafka/KafkaMessageConsumer.java
@@ -15,48 +15,60 @@
  */
 package com.hubrick.vertx.kafka;
 
-import org.vertx.java.busmods.BusModBase;
-import org.vertx.java.core.eventbus.Message;
+import com.google.common.base.Strings;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.json.JsonObject;
 
 /**
  * Vert.x Module to read from a Kafka Topic.
- *
+ * 
  * Created by Marcus Thiesen (mt@hubrick.com) on 04.03.15.
  */
-public class KafkaMessageConsumer extends BusModBase {
+public class KafkaMessageConsumer extends AbstractVerticle {
 
     public static final int DEFAULT_ZOOKEEPER_TIMEOUT_MS = 100000;
     private VertxKafkaConsumer consumer;
     private KafkaConfiguration configuration;
+    private JsonObject config;
 
     @Override
-    public void start() {
+    public void start() throws Exception {
         super.start();
+
+        config = vertx.getOrCreateContext().config();
 
         configuration = KafkaConfiguration.create(
                 getMandatoryStringConfig(KafkaConfiguration.KEY_GROUP_ID),
                 getMandatoryStringConfig(KafkaConfiguration.KEY_KAFKA_TOPIC),
                 getMandatoryStringConfig(KafkaConfiguration.KEY_VERTX_ADDRESS),
                 getMandatoryStringConfig(KafkaConfiguration.KEY_ZOOKEEPER),
-                getOptionalIntConfig(KafkaConfiguration.KEY_ZOOKEPER_TIMEOUT_MS, DEFAULT_ZOOKEEPER_TIMEOUT_MS),
-                getOptionalIntConfig(KafkaConfiguration.KEY_MAX_UNACKNOWLEDGED, 100 ),
-                getOptionalLongConfig(KafkaConfiguration.KEY_MAX_UNCOMMITTED_OFFSETS, 1000 ),
-                getOptionalLongConfig(KafkaConfiguration.KEY_ACK_TIMEOUT_MINUTES, 10 ));
+                config.getInteger(KafkaConfiguration.KEY_ZOOKEPER_TIMEOUT_MS, DEFAULT_ZOOKEEPER_TIMEOUT_MS),
+                config.getInteger(KafkaConfiguration.KEY_MAX_UNACKNOWLEDGED, 100),
+                config.getLong(KafkaConfiguration.KEY_MAX_UNCOMMITTED_OFFSETS, (long) 1000),
+                config.getLong(KafkaConfiguration.KEY_ACK_TIMEOUT_MINUTES, (long) 10));
 
-        consumer = VertxKafkaConsumer.create( configuration, this::handler );
+        consumer = VertxKafkaConsumer.create(configuration, this::handler);
 
         consumer.start();
     }
 
-    private void handler( final String vertxAddress, final String jsonMessage, final Runnable ack ) {
-        vertx.eventBus().send(vertxAddress, jsonMessage, (Message<String> message) -> {
+    private String getMandatoryStringConfig(final String key) {
+        final String value = config.getString(key);
+        if (Strings.isNullOrEmpty(value)) {
+            throw new IllegalArgumentException("No configuration for key " + key + "found");
+        }
+        return value;
+    }
+
+    private void handler(final String vertxAddress, final String jsonMessage, final Runnable ack) {
+        vertx.eventBus().send(vertxAddress, jsonMessage, (result) -> {
             ack.run();
-        } );
+        });
     }
 
     @Override
-    public void stop() {
-        if ( consumer != null ) {
+    public void stop() throws Exception {
+        if (consumer != null) {
             consumer.stop();
         }
         super.stop();
